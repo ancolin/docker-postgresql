6abbf0cc97bcd38d6a9441084cc89cbb53b0f271
ancolin <>
2018-03-19 21:47:05 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
-FROM alpine:3.6
+FROM alpine:3.7
 MAINTAINER ancolin
 
 ENV PGDATA=/var/lib/postgresql/data
 
-RUN apk update && \
-  apk upgrade && \
-  apk add \
+RUN apk update \
+ && apk upgrade \
+ && apk add \
     postgresql \
-    tzdata && \
-  cp /usr/share/zoneinfo/Japan /etc/localtime && \
-  apk del tzdata && \
-  rm -rf /var/cache/apk/* && \
-  echo "export PGDATA=${PGDATA}" > /etc/profile.d/postgresql.sh && \
-  mkdir -p /run/postgresql && \
-  chown postgres:postgres /run/postgresql
+    tzdata \
+ && cp /usr/share/zoneinfo/Japan /etc/localtime \
+ && apk del tzdata \
+ && rm -rf /var/cache/apk/* \
+ && echo "export PGDATA=${PGDATA}" > /etc/profile.d/postgresql.sh \
+ && mkdir -p /run/postgresql \
+ && chown postgres:postgres /run/postgresql
 
-COPY entrypoint.sh /
-ENTRYPOINT ["ash", "entrypoint.sh"]
+COPY entrypoint.sh /usr/bin/
+RUN chmod a+x /usr/bin/entrypoint.sh
+
+USER postgres
+ENTRYPOINT ["entrypoint.sh"]
 CMD ["app:start"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,32 @@
 set -e
 
 case ${1} in
-  app:start)
-    POSTGRES_HBA_CONF=/var/lib/postgresql/data/pg_hba.conf
-    POSTGRES_CONF=/var/lib/postgresql/data/postgresql.conf
-    if [ "${POSTGRES_ALLOW_IPADDRESS}" = '' ]; then
-      POSTGRES_ALLOW_IPADDRESS=0.0.0.0
-    fi
-    if [ "${POSTGRES_ALLOW_SUBNETMASK}" = '' ]; then
-      POSTGRES_ALLOW_SUBNETMASK=0
-    fi
+  app:start|app:backup|app:debug)
 
     case ${1} in
       app:start)
         if [ ! -d ${PGDATA} ]; then
-          su - postgres -c "initdb --encoding utf8 --no-locale -D ${PGDATA}"
+          POSTGRES_HBA_CONF=/var/lib/postgresql/data/pg_hba.conf
+          POSTGRES_CONF=/var/lib/postgresql/data/postgresql.conf
+          if [ "${POSTGRES_ALLOW_IPADDRESS}" = '' ]; then
+            POSTGRES_ALLOW_IPADDRESS=0.0.0.0
+          fi
+          if [ "${POSTGRES_ALLOW_SUBNETMASK}" = '' ]; then
+            POSTGRES_ALLOW_SUBNETMASK=0
+          fi
+          initdb --encoding utf8 --no-locale -D ${PGDATA}
           echo "host  all  all ${POSTGRES_ALLOW_IPADDRESS}/${POSTGRES_ALLOW_SUBNETMASK} trust" >> ${POSTGRES_HBA_CONF}
           echo "listen_addresses = '*'" >> ${POSTGRES_CONF}
         fi
-        su - postgres -c "pg_ctl start -w -D ${PGDATA}"
+        pg_ctl start -w -D ${PGDATA}
+        tail -f /dev/null
+        ;;
+      app:backup)
+        backup_name="${PGDATA}_backup_$(date +'%Y%m%d%H%M%S')"
+        echo "backup PGDATA directory to ${backup_name}"
+        pg_basebackup -D ${backup_name}
+        ;;
+      app:debug)
         tail -f /dev/null
         ;;
       *)
@@ -29,6 +37,7 @@ case ${1} in
   *)
     echo "Available options:"
     echo "  app:start - Start DB cluster."
+    echo "  app:backup - Backup DB cluster."
     ;;
 esac
 
